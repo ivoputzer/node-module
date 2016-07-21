5f088cb8b02fa2805260c734a6fb7446428a1408
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2016-07-21 19:58:44 +0200

    updates various package configurations

--- test/module/config.js
+++ test/module/config.js
@@ -1,7 +1,22 @@
-const {equal} = require('assert')
+const {fail, ok, equal} = require('assert')
+const {exec} = require('child_process')
 
 describe('test/module/config', function () {
-  it('has analytics disabled', () => {
-    equal(process.env.npm_package_config_analyze, 'false')
+  it('disables npm git version tags', (done) => {
+    exec('npm get git-tag-version', is('false').bind(done))
+  })
+  it('disables npm progress bar', (done) => {
+    exec('npm get progress', is('false').bind(done))
+  })
+  it('disables package analytics', () => {
+    const {npm_package_config_analytics:analytics} = process.env
+    equal(analytics, 'false')
   })
 })
+
+function is(expected){
+  return function(err, stdout){
+    equal(stdout.trim(), expected.trim())
+    this()
+  }
+}
